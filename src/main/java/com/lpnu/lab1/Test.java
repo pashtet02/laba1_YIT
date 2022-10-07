c8488acb6d79228b3f9899b35c537bffb5d0bb0f
pashtetgusia <chodak2014ua@gmail.com>
2022-10-07 17:37:53 +0300

    Added unit test

--- src/main/java/com/lpnu/lab1/Test.java
+++ src/main/java/com/lpnu/lab1/Test.java
@@ -1,4 +1,11 @@
 package com.lpnu.lab1;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    public int dummyCode() {
+        return 12;
+    }
 }
---
+++ src/test/java/com/lpnu/lab1/TestTest.java
@@ -0,0 +1,11 @@
+package com.lpnu.lab1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    @org.junit.jupiter.api.Test
+    void dummyCode() {
+        assertEquals(12, new Test().dummyCode());
+    }
+}
